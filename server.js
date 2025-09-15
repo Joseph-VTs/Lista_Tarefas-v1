9610de06ac1d12b27fcda56ae8208bb81a970d25
Joseph-VTs <compcopycode@gmail.com>
2025-09-15 20:57:58 -0300

    Update para Usar Banco de Dados
    Removendo Array, ou seja modelo estático e manual, para um Banco de Dados, processo automático 😀

--- server.js
+++ server.js
@@ -2,77 +2,103 @@
 const 
     Express = require('express'),
     Path = require('path'),
-    App = Express(), // Estância do Express
-    Porta = 3001 // Portas Default 80 ou 8080
+
+    // Estância do Express
+    App_Express = Express(),
+
+    // Portas Default 80 ou 8080
+    Porta = 3001,
+
+    // Banco de Dados -> DB_Produtos_v1
+    My_SQL = require('mysql2')
 ;
 
 // Para poder Acessar "Servir" de forma Statica, arquivos da Pasta Public
-App.use(Express.static(Path.join(__dirname, 'Public')))
-App.use(Express.json())
+App_Express.use(Express.static(Path.join(__dirname, 'Public')));
+App_Express.use(Express.json());
 
 // Rotas e Caminhos
-App.get('/', (req, res) =>{
+App_Express.get('/', (req, res) =>{
     res.sendFile(Path.join(__dirname, 'Public', 'index.html'));
 });
 
 
 
 // Código do Projeto Começa Aqui
+// const
+//     Tabela_Teste = Banco_Dados.query('SELECT TABLE Produtos')
+// ;
 
-let Produtos = [
-    {Nome: "Banana", Produto_Sell: false},
-    {Nome: "Maça", Produto_Sell: true},
-    {Nome: "Bergamota", Produto_Sell: false},
-    {Nome: "Alface", Produto_Sell: true},
-    {Nome: "Uva", Produto_Sell: true}
-];
+const Banco_Dados = My_SQL.createConnection({
+    // Conjunto de Valores | Atributos para a configurar a coneção
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_produtos_v1'
+});
+
+Banco_Dados.connect(err => {
+    if(err) throw err;
+    console.log("Conectado ao Banco de Dados ✅");
+});
 
 // Rotas
-App.get('/Produtos', (req, res) =>{
-    //Devolve o que têm dentro de "Produtos"
-    res.json(Produtos);
+App_Express.get('/Produtos', (req, res) =>{
+    //Devolve o que têm dentro da Tabela "Produtos"
+    Banco_Dados.query(
+        'SELECT * FROM produtos', (Erro_DB, Table_Produtos) => { // Consulta no Banco de Dados
+        if(Erro_DB) throw Erro_DB;
+        res.json(Table_Produtos);
+    }); 
 });
 
-App.post('/Adicionar', (req, res) =>{ //Botão Adicionar do Formulário
+App_Express.post('/Adicionar', (req, res) =>{ //Botão Adicionar do Formulário
     let Nome_Produto = req.body.Limpar_Input_Produto;
-    Produtos.push({Nome: Nome_Produto, Produto_Sell: false});
-    res.json(Produtos);
+    Banco_Dados.query(
+        'INSERT INTO produtos (Nome, Produto_Sell) VALUES (?, false)', /*? = Baind*/[Nome_Produto], Erro_DB => {
+        if(Erro_DB) throw Erro_DB;
+        res.json("Ok ✅, Conecção com a Tabela Produtos!");
+    });
 });
 
-App.post('/Editar', (req, res) => {
+App_Express.post('/Editar', (req, res) => {
     const
         Nome_Antigo = req.body.Nome_Antigo,
         Nome_Novo = req.body.Nome_Novo
     ;
 
-    for(let Produto of Produtos){
-        if(Produto.Nome === Nome_Antigo){
-            Produto.Nome = Nome_Novo;
-        }
-    }
-    res.json(Produtos);
+    Banco_Dados.query(
+        'UPDATE produtos SET Nome = ? WHERE Nome = ?',
+        [Nome_Novo, Nome_Antigo], Erro_DB =>{
+            if(Erro_DB) throw Erro_DB;
+            res.sendStatus(200);
+        });
 });
 
-App.post('/Mudar_Status', (req, res) => {
+App_Express.post('/Mudar_Status', (req, res) => {
     const
         Nome_Produto = req.body.Nome_Produto
     ;
 
-    for(let Produto of Produtos){
-        if(Produto.Nome === Nome_Produto){
-            Produto.Produto_Sell = !Produto.Produto_Sell;
-        }
-    }
-    res.json(Produtos);
+    Banco_Dados.query(
+        'UPDATE produtos SET Produto_Sell = Not Produto_Sell WHERE Nome = ?',
+        [Nome_Produto], Erro_DB =>{
+            if(Erro_DB) throw Erro_DB;
+            res.sendStatus(200);
+        });
 });
 
-App.post('/Excluir', (req, res) =>{
+App_Express.post('/Excluir', (req, res) =>{
     const
         Nome_Produto = req.body.Nome_Produto
     ;
 
-    Produtos = Produtos.filter(Produto => Produto.Nome !== Nome_Produto);
-    res.json(Produtos);
+    Banco_Dados.query(
+        'DELETE FROM produtos WHERE Nome = ?',
+        [Nome_Produto], Erro_DB =>{
+            if(Erro_DB) throw Erro_DB;
+            res.sendStatus(200);
+        });
 });
 
 // Código do Projeto Termina Aqui
@@ -80,7 +106,7 @@ App.post('/Excluir', (req, res) =>{
 
 
 // Escuta a Porta!
-App.listen(
+App_Express.listen(
     Porta, () =>{
         console.log("Servidor Conectado ✅" + "\nLink: http://127.0.0.1:3001/");
     }
